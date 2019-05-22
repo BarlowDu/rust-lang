b96143a64034b355138278de2e96d2ccc48b32a4
dutc <dutc@yiche.com>
2019-05-22 18:07:18 +0800

    add docs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
-mod module;
-mod irun;
 
-use module::ilifetime;
-use module::ienum;
-use module::istring;
+mod irun;
+mod module;
 use module::iborrow;
+use module::ienum;
+use module::ilifetime;
+use module::istring;
+
+use module::iborrow::Runner;
+use core::ops::Deref;
+
 
 use irun::IRunner;
 
 fn main() {
-    let mut runners:Vec<Box<dyn IRunner>>=Vec::new();
+    let mut runners: Vec<Box<dyn IRunner>> = Vec::new();
     runners.push(Box::new(ilifetime::Runner::new()));
     runners.push(Box::new(ienum::Runner::new()));
     runners.push(Box::new(istring::Runner::new()));
     runners.push(Box::new(iborrow::Runner::new()));
-    for runner in runners{
-        runner.run()
-    } 
+    for runner in runners {
+        //&runner.deref()
+        //run(runner.deref());
+        runner.run();
+    }
+   
+}
+
+fn run(runner: &impl irun::IRunner) {
+    runner.run();
 }
 
--- src/module/iborrow.rs
+++ src/module/iborrow.rs
@@ -11,6 +11,7 @@ impl Runner{
 impl irun::IRunner for Runner{
     fn run(&self){
         testReference();
+        testBorrow();
         testMutBorrow();
     }
 }
@@ -22,10 +23,37 @@ fn testReference(){
     //s.clear(); //error
     println!("{}",*s1)
 }
+/*
+当mut变量a被不可变(b)引用后,
+a将变成不可变(条件是b在a变更后被使用),只到b被释放
+
+当b引用了a,但a发生了变更,那么b可能将无法正确获取到数据.
+无同步机制,或者无需同步机制
+ */
+fn testBorrow(){
+    let mut s=String::from("hello rust");
+    {
+        let s1=&s;
+        s.push_str("!");
+        //println!("{}",s1);//如果s1被使用,那么上一句将不能编译通过(complie error)
+    }
+
+
+}
 fn testMutBorrow(){
     let mut s=String::from("rust language");
-    let s2=&mut s;
-    s2.push_str(" hello");
-    println!("s:{}",s);
-    //println!("s2:{}",s2); //error
+    {
+        let s2=&mut s;
+        //let s3=&s; //complie error
+        let s4=&s2;//这一句之所有能够编译通过,是因为s2在这时可能变成了只读;  
+        let l=s4.len();  
+        s2.push_str(" !");
+        println!("s2:{}",s2);
+        println!("s:{}",s);
+        //上两句顺序为何不能互换:
+        //一个可变变量只能有一个可变引用,或者多个不可变引用
+        //println!("s:{}",s) 会传递s的一个不可用引用
+        
+        //let l=s4.len();    
+    }
 }
